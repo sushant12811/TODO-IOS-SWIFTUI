400cafdc0f20e0fbfaf5f7d42e814a3770ad0923
sushant12811 <steve.malistella@gmail.com>
2025-04-02 11:45:30 -0400

    added pickerView and button to add task

--- ToDoListApp/AddTaskView.swift
+++ ToDoListApp/AddTaskView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct AddTaskView: View {
     
     @State private var textFieldValue: String = ""
+    var item = ["Normal", "Optional", "Urgent"]
+    @State private var initialSelection: String = "Normal"
     
     var body: some View {
-        VStack(alignment:.leading){
+        VStack(alignment:.leading, spacing: 15){
             textModifier(title: "Task Title")
             textField(placeHolder: "Task Title", textValue: $textFieldValue)
             textModifier(title: "Priority")
+            pickerView(items: item, selection: $initialSelection)
+            addButton(title: "Add Task"){
+                
+            }
+            
+            Spacer()
             
         }
         .padding()
@@ -45,6 +53,34 @@ extension AddTaskView {
                     .stroke(lineWidth: 1)
                     .foregroundStyle(Color.gray.opacity(0.4))
             }
+    }
+    
+    //PickerView
+    @ViewBuilder
+    func pickerView(items: [String], selection: Binding<String>) -> some View {
+        Picker("Select Priority", selection: selection) {
+            ForEach(items, id: \.self) {
+                Text($0)
+            }
+        }
+    }
+    
+    //Add Button
+    @ViewBuilder
+    func addButton(title: String, onTap : @escaping () -> Void) -> some View {
+        Button{
+            onTap()
+        }label: {
+            Text(title)
+                .foregroundStyle(.white)
+                .font(.headline)
+                .fontWeight(.semibold)
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(.green)
+        .clipShape(.rect(cornerRadius: 7))
+        .padding()
     }
 }
 
